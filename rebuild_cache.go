760485460657ae22ea09626ba2ac7a5a8e605fe1
kateBy <nonameisiam@yandex.ru>
2016-03-02 08:57:57 +0300

    Функция загрузки po-файла вынесена в отдельный модуль. Начата разработка функции поиска строк в исполняемом файле

--- rebuild_cache.go
+++ rebuild_cache.go
@@ -1,49 +1,29 @@
 package main
 
 import (
+	"debug/elf"
 	"fmt"
-	"io/ioutil"
-	"strings"
 )
 
-func main(){
-	strings := loadString("trans.po")
-	fmt.Println(strings)
+func main() {
+	//strings := loadString("trans.po")
+	//fmt.Println("Hello")
+	extractStrings("Dwarf_Fortress")
+
 }
 
-func loadString(fileName string) map[string]string {
-	buf, err := ioutil.ReadFile(fileName) 
+func extractStrings(fileName string) { //[]string {
+
+	elfFile, err := elf.Open(fileName)
 	if err != nil {
-		fmt.Println("Ошибка при открытии файла", fileName)
+		fmt.Println("Ошибка при открытии", fileName)
 	}
-	
-	lines := strings.Split(string(buf), "\n")
-	max_lines := len(lines) - 1
-	
-	var i int
-	var pos int
-	var msgid string
-	var msgstr string
-    result := make(map[string]string)
-	
-	for {
-		if i == max_lines {
-			break
-		}
-		
-		if strings.HasPrefix(lines[i], "msgid") {
-			pos = strings.Index(lines[i],"\"") + 1
-			msgid = lines[i][pos:len(lines[i])-1]
-			i++
-			pos = strings.Index(lines[i],"\"") + 1
-			msgstr = lines[i][pos:len(lines[i])-1]
-			
-			result[msgid] = msgstr
-			
-		}
-		
-		i++
+	defer elfFile.Close()
+
+	rodata := elfFile.Section(".rodata")
+	if rodata != nil {
+		data, err := rodata.Data()
+		fmt.Println(len(data))
 	}
-		
-	return result
+
 }
